85c450b94260797de183ce98a302ccbf5d26a233
guarim <84085689+guarim@users.noreply.github.com>
2022-01-03 12:13:41 +0100

    Update header.js

--- components/header/header.js
+++ components/header/header.js
@@ -37,7 +37,7 @@ class PageHeader extends HTMLElement {
                 }
             </style>
 
-            <a href="/Studio-ARJS-LPJulesVerne">
+            <a href="/Studio-ARJS-LPJulesVerne.io">
                 <img class="logo-img" alt="logo" />
             </a>
         `;
